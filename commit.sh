6e3085244bd4d42b46af919384286289a935ff5b
Cyrille Le Clerc <cleclerc@cloudbees.com>
2018-09-11 15:18:05 +0200

    Better commit script

--- commit.sh
+++ commit.sh
@@ -3,24 +3,25 @@
 
 export JIRA_PROJECT_KEY="WOR"
 export CHANGELOG_FILE="CHANGELOG"
+export JIRA_URL="https://jira.beescloud.com"
 
 # https://developer.atlassian.com/jiradev/jira-apis/jira-rest-apis/jira-rest-api-tutorials/jira-rest-api-example-create-issue
 
-export FEATURE_DESC="Feature $(date)"
+export FEATURE_DESC="Front feature $(date)"
 
 export JIRA_ISSUE_AS_JSON=$(curl --silent --netrc -H "Content-Type: application/json" -X POST \
    --data "{\"fields\":{\"project\":{\"key\":\"$JIRA_PROJECT_KEY\"},\"summary\":\"$FEATURE_DESC\",\"description\":\"We are working on $FEATURE_DESC\",\"issuetype\":{\"name\":\"New Feature\"}}}" \
-   https://jira.beescloud.com/rest/api/2/issue/)
+   $JIRA_URL/rest/api/2/issue/)
 
 
 export JIRA_ISSUE=$(echo $JIRA_ISSUE_AS_JSON | jq -r .key)
 
-echo "Issue $JIRA_ISSUE created"
+echo "Issue $JIRA_URL/browse/$JIRA_ISSUE created: $FEATURE_DESC"
 
 # TODO update issue, set status to "in progress"
 # see https://docs.atlassian.com/jira/REST/server/?_ga=2.148590398.82631418.1503650538-983538049.1489593855#api/2/issue-doTransition
 
-export COMMIT_MESSAGE="$JIRA_ISSUE change"
+export COMMIT_MESSAGE="$JIRA_ISSUE Front feature change ..."
 
 git pull
 
@@ -33,4 +34,4 @@ git push
 
 
 echo "## SUCCESS #"
-echo "Commit for $JIRA_ISSUE pushed"
+echo "Commit for $JIRA_URL/browse/$JIRA_ISSUE pushed"
